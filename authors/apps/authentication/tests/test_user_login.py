87a9ac35b33a4d43a9a1b87eadb44f51f5d0a339
asheuh <asheuh49@egmail.com>
2018-10-23 14:14:12 +0300

    [Chore #161278892] Set up tests functionality for the application

---
+++ authors/apps/authentication/tests/test_user_login.py
@@ -0,0 +1,31 @@
+from authors.base_test import BaseTestCase
+
+class TestUserLogin(BaseTestCase):
+    """
+    This class logs in a user who has an account in the application
+    params: [email, password]
+    """
+
+    def test_user_login(self):
+        with self.client:
+            pass
+
+    def test_user_login_with_invalid_password(self):
+        with self.client:
+            pass
+
+    def test_user_login_with_no_email(self):
+        with self.client:
+            pass
+
+    def test_user_login_with_no_password(self):
+        with self.client:
+            pass
+
+    def test_user_login_without_an_account(self):
+        with self.client:
+            pass
+
+    def test_user_login_with_deactivated_account(self):
+        with self.client:
+            pass
