a54c8e55e5bf8b9da6708884ec7e4d6b7091428f
asheuh <asheuh49@egmail.com>
2018-10-23 15:50:56 +0300

    [Chore #161278892] Set up tests functionality for the application

--- authors/apps/authentication/tests/test_user_register.py
+++ authors/apps/authentication/tests/test_user_register.py
@@ -1,3 +1,5 @@
+from django.urls import reverse
+from rest_framework import status
 from authors.base_test import BaseTestCase
 
 class TestUserRegistration(BaseTestCase):
@@ -6,8 +8,13 @@ class TestUserRegistration(BaseTestCase):
     Params: [username, email, password]
     """
     def test_user_creates_account(self):
+        """
+        Ensures user can create a new account
+        """
         with self.client:
-            pass
+            url = reverse('create_user')
+            response = self.client.post(url, self.new_user, format='json')
+            self.assertEqual(response.status, status.HTTP_201_CREATED)
 
     def test_user_register_with_invalid_email(self):
         with self.client:
