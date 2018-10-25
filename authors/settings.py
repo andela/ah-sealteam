94291d98c58101181be60185ce4266b1f5b9bfbb
rickynyairo <rickynyairo@gmail.com>
2018-10-25 12:24:23 +0300

    [ch #161474387] added static root setting for heroku deploy

--- authors/settings.py
+++ authors/settings.py
@@ -122,11 +122,18 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 CORS_ORIGIN_WHITELIST = (
     '0.0.0.0:4000',
     'localhost:4000',
